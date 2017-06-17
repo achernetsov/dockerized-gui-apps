4734b6456704c60e43020d6d6d8ab39880479c69
achernetsov <chernetsov.artyom@gmail.com>
2017-06-17 07:09:51 +0300

    rename alsa ctr

--- alsa/playsound.sh
+++ alsa/playsound.sh
@@ -1,4 +1,4 @@
 # build: docker build -t alsa .
 # usage: ./playsound.sh <sound file name>
 # example:  ./playsound.sh meow.wav (meow.wav should be present in ./resources folder)
-docker run --rm --device /dev/snd -v $(pwd)/resources:/sounds alsa /sounds/$1
+docker run --rm --device /dev/snd -v $(pwd)/resources:/sounds achernetzov/alsa /sounds/$1

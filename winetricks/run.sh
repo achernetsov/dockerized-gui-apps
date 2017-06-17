465d37ea8f86a25f3fed21951a04d5ba732a1542
achernetsov <chernetsov.artyom@gmail.com>
2017-06-17 16:48:19 +0300

    fixed winetricks run, steam windows game run successfully

--- winetricks/run.sh
+++ winetricks/run.sh
@@ -3,6 +3,11 @@
 xhost +
 nvidia-docker run --rm -it \
 --device /dev/snd \
+--device /dev/nvidia0 \
+--device /dev/nvidiactl \
+--device /dev/nvidia-modeset \
+--device /dev/nvidia-uvm \
+--device /dev/nvidia-uvm-tools \
 -e DISPLAY=$DISPLAY \
 -v /tmp/.X11-unix:/tmp/.X11-unix \
 -v $XAUTHORITY:/tmp/.host_Xauthority:ro \

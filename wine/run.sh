33ff6fe73d4768cd7c793ea0491ba86a96499924
achernetsov <chernetsov.artyom@gmail.com>
2017-06-17 15:20:25 +0300

    wine & winetricks

---
+++ wine/run.sh
@@ -0,0 +1,14 @@
+# create steam-data in this directory. steam files will be here 
+
+xhost +
+nvidia-docker run --rm -it \
+--device /dev/snd \
+-e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $XAUTHORITY:/tmp/.host_Xauthority:ro \
+-v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket:ro \
+-v /dev/shm:/dev/shm \
+-v /etc/machine-id:/etc/machine-id \
+--net=host \
+--shm-size="4G" \
+ achernetzov/wine /bin/bash
---
+++ winetricks/run.sh
@@ -0,0 +1,15 @@
+# create steam-data in this directory. steam files will be here 
+
+xhost +
+nvidia-docker run --rm -it \
+--device /dev/snd \
+-e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $XAUTHORITY:/tmp/.host_Xauthority:ro \
+-v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket:ro \
+-v /dev/shm:/dev/shm \
+-v /etc/machine-id:/etc/machine-id \
+-v $(pwd)/wine-data:/root/.wine \
+--net=host \
+--shm-size="4G" \
+ achernetzov/winetricks /bin/bash

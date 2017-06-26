d99aad2af165ff52976afd42f471c52823597dc6
achernetsov <chernetsov.artyom@gmail.com>
2017-06-26 03:40:21 +0300

    added blizard image, battle net works

---
+++ games/blizzard/README.md
@@ -0,0 +1,5 @@
+Install corefonts, vcrun2015 and dotnet40 (only needed if you want to run hearthstone). Run in winxp default. 32 bit prefix.
+
+source: https://appdb.winehq.org/objectManager.php?sClass=version&iId=28855&iTestingId=98031
+
+worked with wine-2.11 (devel)
---
+++ games/blizzard/run.sh
@@ -0,0 +1,19 @@
+nvidia-docker run --rm -it \
+--device /dev/snd \
+--device /dev/nvidia0 \
+--device /dev/nvidiactl \
+--device /dev/nvidia-modeset \
+--device /dev/nvidia-uvm \
+--device /dev/nvidia-uvm-tools \
+-e DISPLAY=$DISPLAY \
+-v /tmp/.X11-unix:/tmp/.X11-unix \
+-v $XAUTHORITY:/tmp/.host_Xauthority:ro \
+-v /var/run/dbus/system_bus_socket:/var/run/dbus/system_bus_socket:ro \
+-v /dev/shm:/dev/shm \
+-v /etc/machine-id:/etc/machine-id \
+-v /etc/localtime:/etc/localtime \
+-v /etc/network/interfaces:/etc/network/interfaces \
+-v $(pwd)/../../wine-home:/home/wine \
+--net=host \
+--shm-size="4G" \
+ achernetsov/blizzard /bin/bash
